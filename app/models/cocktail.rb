c91714b674079dfb95fa741f560cc6d6f1afb001
Nate Gunawan <nategunawan@gmail.com>
2020-11-27 12:51:25 +0800

    next

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,8 @@
 class Cocktail < ApplicationRecord
+  has_one_attached :photo
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true
+  validates :description, presence: true
   validates :name, uniqueness: true
 end
